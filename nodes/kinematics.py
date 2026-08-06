1b90eb95180411fd034195d5da0f43693c7f42b2
Andrew <andrew.wlsn01@gmail.com>
2026-08-06 18:07:27 -0400

    Revert "Minor changes to kinematics.py"

--- nodes/kinematics.py
+++ nodes/kinematics.py
@@ -1,70 +1,59 @@
 import pandas as pd
 import pygetwindow as gw
+from pynput import keyboard
 from datetime import datetime
-import zenoh, json, time, keyboard, config
+import zenoh, json, time, config
 
 data_recieved_flag = False
-system_exit_flag = False
+system_exit_flag = True
 projectile_shot_flag = False
 manual_intervention_flag = False
 
-data_collection_mode = True
-training_mode = False
-autonomous_mode = False
+data_collection_flag = True
+training_flag = False
+autonomous_flag = False
 
 object_px_height = None
 camera_height = None
 servo_y_pos = 90
 
-def onShot():
-    global projectile_shot_flag
-    projectile_shot_flag = True
-
-def onESC():
-    global system_exit_flag
-    system_exit_flag = True
-
 def initDatabases():
     global kinematics_dataset, kinematics_training_data
 
-    print("\nDatabases initializing...")
-
-    if data_collection_mode is True:
+    if data_collection_flag is True:
         print(config.data_collection_message)
         kinematics_dataset = pd.DataFrame(columns=["Object Pixel Height", "Camera Height", "Servo Angle"])
 
-    elif training_mode is True:
+    elif training_flag is True:
         print(config.training_message)
         kinematics_dataset = pd.read_csv("")
         kinematics_training_data = pd.DataFrame(columns=["Object Pixel Height", "Camera Height", "Predicted Servo Angle", "Actual Servo Angle"])
 
-    elif autonomous_mode is True:
+    elif autonomous_flag is True:
         print(config.autonomous_message)
         kinematics_dataset = pd.read_csv("")
 
 def deinitDatabases():
     global kinematics_dataset, kinematics_training_data
 
-    if data_collection_mode is True:
-        kinematics_dataset.to_csv("C:/dev/autonomous_turret_v1/database/kinematics_dataset.csv",index=False)
+    if data_collection_flag is True:
+        kinematics_dataset.to_csv("database/kinematics_dataset.csv",index=False)
 
-    elif training_mode is True:
-        kinematics_dataset.to_csv("C:/dev/autonomous_turret_v1/database/kinematics_dataset.csv",index=False)
-        kinematics_training_data.to_csv("C:/dev/autonomous_turret_v1/database/kinematics_training_dataset.csv",index=False)
+    elif training_flag is True:
+        kinematics_dataset.to_csv("database/kinematics_dataset.csv",index=False)
+        kinematics_training_data.to_csv("database/kinematics_dataset.csv",index=False)
 
-    elif autonomous_mode is True:
-        kinematics_dataset.to_csv("C:/dev/autonomous_turret_v1/database/kinematics_dataset.csv",index=False)
-
-    print("\nDatabases exported.")
+    elif autonomous_flag is True:
+        kinematics_dataset.to_csv("database/kinematics_dataset.csv",index=False)
 
 def predictionModel():
     return 0
 
 def main():
     global object_px_height, camera_height
-    global data_recieved_flag, data_collection_mode, training_mode, autonomous_mode, projectile_shot_flag, manual_intervention_flag
+    global data_recieved_flag, data_collection_flag, training_flag, autonomous_flag, projectile_shot_flag, manual_intervention_flag
 
-    if data_collection_mode and projectile_shot_flag is True:
+    if data_collection_flag and projectile_shot_flag is True:
         print(f"\nRecent shot was at object pixel height: {object_px_height}, camera_height: {camera_height}, servo angle: {servo_y_pos}.")
 
         ans = input("Enter (y/n) to add entry to database: ").lower()
@@ -73,13 +62,11 @@ def main():
         if ans == "n":
             print("\nEntry ignored.")
 
-        projectile_shot_flag = False
-
-    if training_mode or autonomous_mode is True:
+    if training_flag or autonomous_flag is True:
         servo_angle = predictionModel()
         sendServoYJSON(servo_angle)
 
-        if training_mode and projectile_shot_flag is True:
+        if training_flag and projectile_shot_flag is True:
             print(f"\nModel prediction was at object pixel height: {object_px_height}, camera_height: {camera_height}, servo angle: {servo_y_pos}.")
             
             ans = input("Was the prediction correct (y/n): ").lower()
@@ -87,21 +74,17 @@ def main():
                 kinematics_dataset.loc[len(kinematics_dataset)] = [object_px_height, camera_height, servo_y_pos]
             if ans == "n":
                 manual_intervention_flag = True
-                projectile_shot_flag = False
                 print("\nManual intervention taking place: ")
 
                 while manual_intervention_flag is True:
-                    if projectile_shot_flag is True:
-                        print(f"\nRecent shot was at object pixel height: {object_px_height}, camera_height: {camera_height}, servo angle: {servo_y_pos}.")
-                    
-                        ans = input("Enter (y/n) to add entry to database: ").lower()
-                        if ans == "y":
-                            kinematics_dataset.loc[len(kinematics_dataset)] = [object_px_height, camera_height, servo_y_pos]
-                            manual_intervention_flag = False
-                        if ans == "n":
-                            print("\nEntry ignored.")
-
-                        projectile_shot_flag = False
+                    print(f"\nRecent shot was at object pixel height: {object_px_height}, camera_height: {camera_height}, servo angle: {servo_y_pos}.")
+                
+                    ans = input("Enter (y/n) to add entry to database: ").lower()
+                    if ans == "y":
+                        kinematics_dataset.loc[len(kinematics_dataset)] = [object_px_height, camera_height, servo_y_pos]
+                        manual_intervention_flag = False
+                    if ans == "n":
+                        print("\nEntry ignored.")
 
         data_recieved_flag = False
 
@@ -138,9 +121,6 @@ if __name__ == "__main__":
         keyboard_controls_sub = session.declare_subscriber(config.keyboard_controls, keyboard_controls_cb)
 
         initDatabases()
-
-        keyboard.add_hotkey('esc', onESC)
-        keyboard.add_hotkey('p', onShot)
 
         while True:
             time.sleep(0.01)
